6613852fc249e276115f517a9cc46f5ad7e37757
Philip Chow <pchow101@yahoo.com>
2016-05-21 17:07:50 -0400

    Update
    Update

--- map.js
+++ map.js
@@ -1,14 +1,33 @@
 var map;
-		require(["esri/map", "dojo/domReady!", "esri/geometry/Extent", "esri/SpatialReference"],
-			function(Map)
-			{
-				map = new Map("mapDiv",
-				{
-					center: [-77.09, 38.96325],
-					zoom: 15,
-					basemap: "streets",
-					slider: true,
-					logo: false
-				} );
-			}
-		);
+
+require(["esri/map",
+	"esri/geometry/Extent",
+	"esri/SpatialReference",
+	"esri/layers/FeatureLayer",
+	"dojo/domReady!"
+	],
+	
+	function(Map, Extent, FeatureLayer)
+	{
+		// Load map centered on Friendship Heights - approx. coord 77.09 W, 38.96325 N
+		var mapBox = new Extent({"xmin":-77.14,"ymin":38.91325,"xmax":-77.04,"ymax":39.01325,"spatialReference":{"wkid":4326} });
+		
+		map = new Map("mapDiv",
+		{
+			// Define map viewport			
+			extent: mapBox,
+			zoom: 15,
+			slider: true,
+			showLabels : true,
+			logo: false,
+			
+			// Map type -- Use streets or hybrid
+			basemap: "streets"
+		} );
+		
+		// Feature Layer
+		var featureLayer = new FeatureLayer("https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0");
+
+    map.addLayer(featureLayer);
+	}
+);
