6c9732bec7eb902a07dc2af92187c7e73461dbdc
Leland Jordon <lelandjordon@mac.com>
2016-05-22 12:28:13 -0400

    forgot what I did

--- controllers/response_control.js
+++ controllers/response_control.js
@@ -2,29 +2,29 @@ var routerApp = angular.module('routerApp', ['ui.router', 'ngAnimate', 'ui.boots
  'userApp']);
 
 routerApp.config(function($stateProvider, $urlRouterProvider) {
-    
+
     $urlRouterProvider.otherwise('/home');
-    
+
     $stateProvider
-        
+
         // HOME STATES ========================================
         .state('home', {
             url: '/home',
-            templateUrl: 'pages/home.html'
-            
+            templateUrl: 'pages/home.html',
+            controller:  'responseController',
+            controllerAs: 'resCon'
+
         })
-        
+
         // POSITION PAGE=================================
         .state('user', {
             url: '/user',
             views: {
-                '': { templateUrl: 'pages/user.html', 
+                '': { templateUrl: 'pages/user.html',
                       controller: 'userController' },
 
             }
-            
+
         })
 
 });
-
-
